dabccadfa0aec1f06d78797c1484832453a9a0cf
KJ <kaityagalvan@gmail.com>
2019-10-12 17:04:16 -0700

    Added firebase to app

--- src/components/screens/Product.js
+++ src/components/screens/Product.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import firebase from 'firebase';
+import '@firebase/firestore';
 
 export default class Product extends React.Component {
   render() {
+    const dbh = firebase.firestore();
+
+    dbh.collection("products").doc("test").set({
+      name: "hi"
+    })
+    .then(function() {
+    })
+    .catch(function(error) {
+    });
+
     const { navigation } = this.props;
     return (
       <View style={styles.container}>
