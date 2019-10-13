39e19b5e4cd1464f6a7a890d29327c578924bb01
shuishuini <nydwhu@gmail.com>
2019-10-12 17:54:59 -0700

    fetched product from firebase

--- src/components/screens/Product.js
+++ src/components/screens/Product.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import firebase from 'firebase';
 import '@firebase/firestore';
 
 export default class Product extends React.Component {
   render() {
-    const dbh = firebase.firestore();
+    const { navigation } = this.props;
 
-    dbh.collection("products").doc("test").set({
-      name: "hi"
-    })
-    .then(function() {
+    const db = firebase.firestore();
+    const barcode = navigation.getParam('productBarCode', '')
+
+    console.log("hi2");
+
+    db.collection("products").where("barcode", "==", barcode)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
     })
     .catch(function(error) {
+        console.log("Error getting documents: ", error);
     });
 
-    const { navigation } = this.props;
     return (
       <View style={styles.container}>
         <Text>Type {JSON.stringify(navigation.getParam('barCodeType', 'no type'))}</Text>
-        <Text>Type {JSON.stringify(navigation.getParam('productBarCode', '00'))}</Text>
+        <Text>Type {barcode}</Text>
       </View>
     );
   }
