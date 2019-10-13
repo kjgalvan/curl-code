7d094df99425d852ecc80a9657a88fbf6f4862fb
Yidan-Ni <yidan.ni@wsu.edu>
2019-10-12 21:04:18 -0700

    added create user

---
+++ src/components/screens/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Profile Screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default Profile
